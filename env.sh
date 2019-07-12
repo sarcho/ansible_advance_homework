d4b078b3ae7f741753153c4167867446db966983
Sarcho <daniel.tejada@cerereceda.de>
2019-07-12 11:22:12 +0200

    Fixes
    Completed roles

---
+++ env.sh
@@ -0,0 +1,15 @@
+# Laptop
+export TOWER_GUID=13cd
+export MYKEY=~/.ssh/id_rsa
+export MYUSER=daniel.tejada.cereceda-devoteam
+
+
+# Bastion
+export TOWER_GUID=13cd
+export OSP_GUID=afc0
+export OPENTLC_LOGIN=daniel.tejada.cereceda-devoteam
+export OPENTLC_PASSWORD='kR3g6imJ!7GN065LPj5I'
+export GITHUB_REPO=https://github.com/sarcho/ansible_advance_homework
+export JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
+export REGION=us-east-1
+export RH_MAIL_ID=daniel.tejada.cereceda
